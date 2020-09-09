43d5c4af93fa7a7f7345cfd4bd4d0bcffe69a5d5
g5sh <eagomez2387@gmail.com>
2020-09-09 01:47:53 -0500

    [ADD] Bases: Add 01 const and let

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,14 @@
+// Variables and const in js
+
+const name = "Helena";
+const lastname = "Gomez";
+
+let randon = 5;
+randon = 10;
+
+console.log(name, lastname, randon)
+
+if (true) {
+    let randon = 1;
+    console.log(randon)
+}
